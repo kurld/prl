d37488f606cedac7299bf06df49a65c18f04a1a0
kurld <lw@c-4.pl>
2014-10-30 11:22:42 +0100

    init4

--- logparser/apache_log_parser.pl
+++ logparser/apache_log_parser.pl
@@ -1,54 +1,70 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
 use Getopt::Std;
 sub croak {die "$0: @_: $!\n"}
-my %ha = ();
-my %ha2 = ();
-my %opts = ();
-getopts("ab", \%opts);
+my %ha;
+my %opts;
+my $pattern;
+my $info = "To moze chwile potrwac...";
+getopts("abc", \%opts);
 
-sub read_file {
-
-    my $file = shift;
-    open(FILE, $file) or croak;
-    while (my $line = <FILE>) {
-        if ($line =~ m/$main::pattern/) {
-            if ( !$ha{$1}) {
-                $ha{$1}++;
-            }
-        }
-    }
-    close FILE;
-}    
- 
-sub openx {
+sub ip_num {
+    print STDERR $info, "\n";
     while (my $files = shift) {
-
-        read_file $files;
+	open(FILE, $files) or croak;
+	while (my $line = <FILE>) {
+	    if ($line =~ m/$pattern/) {
+		$ha{$1}++ if (!$ha{$1}); 
+	    }
+	}
+	close FILE;
     }
-}
+    print my $size = keys %ha, " unikalnych IP\n";
+}  
+  
+sub ip_max {
+    print STDERR $info, "\n";
+    while (my $files = shift) {
+	open(FILE, $files) or croak;
+	while (my $line = <FILE>) {
+	    if ($line =~ m/$pattern/) {
+		$ha{$1}++ ; 
+	    }
+	}
+	close FILE;
+    }
+    foreach my $key (sort {$ha{$a} <=> $ha{$b}} keys %ha) {
+	print "$key: $ha{$key}\n";
+    }
+}  
+
+sub browser {
+    print STDERR $info, "\n";
+    while (my $files = shift) {
+	open(FILE, $files) or croak;
+	while (my $line = <FILE>) {
+	    if ($line =~ m/$pattern/) {
+		$ha{$1}++ ; 
+	    }
+	}
+	close FILE;
+    }
+    foreach my $key (sort {$ha{$a} <=> $ha{$b}} keys %ha) {
+	print "$key: $ha{$key}\n";
+    }
+}  
+
 
 sub help {
     print "\nUzycie: perl $0 param plik(i)\n";
     print "   -a podsumowanie IP\n   -b podsumowanie przeglarek\n\n";
 }
 
-if (defined $opts{b}) {
-    our $pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}.*((MSIE|Mozilla).*?);';
-    openx @ARGV
-}
+if (defined $opts{b}) { $pattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}.*((MSIE|Mozilla).*?);'; ip_num @ARGV }
+elsif (defined $opts{a}) { $pattern = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'; ip_num @ARGV }
+elsif (defined $opts{c}) { $pattern = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'; ip_max @ARGV }
+else { help }
 
-elsif (defined $opts{a}) {
-    our $pattern = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})';
-    openx @ARGV
-}
 
-else {
-    help
-}
-
-foreach my $key (sort {$ha{$a} <=> $ha{$b}} keys %ha) {
-	print "$key: $ha{$key}\n";
-}  
-  
   
