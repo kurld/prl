0afb8a6bfde040edf567c17f49217113b23f1add
kurld <lw@c-4.pl>
2014-11-09 00:19:57 +0100

    parser

--- logparser/apache_log_parser.pl
+++ logparser/apache_log_parser.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/env perl
+
 use strict;
 use warnings;
 use Getopt::Std;
 use List::Util 'sum';
+
 sub croak {die "$0: @_: $!\n"}
 
-my %ha;
-my %opts;
-my $pattern;
-my $info = "To moze chwile potrwac...";
-getopts("abc", \%opts);
+my (%ha, %opts, $pattern);
 
-sub ip_num {
+getopts("abcd", \%opts);
+
+sub uniq_ip {
     #print STDERR $info, "\n";
     while (my $files = shift) {
 	open(FILE, $files) or croak;
@@ -23,7 +23,28 @@ sub ip_num {
 	close FILE;
     }
     print my $size = keys %ha, " unikalnych IP\n";
+}
+
+
+sub link_freq {
+    #print STDERR $info, "\n";
+    while (my $files = shift) {
+	open(FILE, $files) or croak;
+	while (my $line = <FILE>) { 
+	    if ($line =~ m/(?:"\S+)\ (\S+)[" ]/) {
+		my $fs = $1;
+		$fs =~ s/"//g;
+		$ha{$fs}++;
+	    }
+	}
+	close FILE;
+    }
+    for my $key (sort {$ha{$a} <=> $ha{$b}} keys %ha) {
+	print "$ha{$key}\t$key\n";
+    }
 }  
+
+
   
 sub ip_max {
     #print STDERR $info, "\n";
@@ -34,8 +55,9 @@ sub ip_max {
 		$ha{$1}++ ; 
 	    }
 	}
-	close FILE;
+	close FILE;	
     }
+    
     my @val = sort( {$ha{$a} <=> $ha{$b}} keys %ha);
     for my $i (-20..-1) {
 	my $hostname = qx/nslookup $val[$i]/;
@@ -46,37 +68,37 @@ sub ip_max {
 	else {
 	    print $val[$i], "\t", $ha{$val[$i]}, "\n";
 	}
-    }  
+    }
 }
 
-
 sub time_of_day {
-    open (my $file, '<', $ARGV[0]) or die $!;
-    while (my $line = <$file>) { 
-	if ($line =~ m/[^\d](\d\d:)/) {
-	    $ha{$1}++;
-	} 
+    while (my $files = shift) {
+	open(FILE, $files) or croak;
+	while (my $line = <FILE>) {
+	    if ($line =~ m/[^\d](\d\d:)/) {
+		$ha{$1}++;
+	    } 
+	}
+	close FILE;
     }
-    close $file;
     for my $key (sort keys %ha) {
-	print "$key ", $ha{$key},"req\t->\t", int($ha{$key} * 100 / sum values %ha), "%\n";  
+	print "$key ", $ha{$key}," \t->\t", int($ha{$key} * 100 / sum values %ha), "%\n";  
     }    
 }
 
-
-
-
 sub help {
     print "\nUzycie: perl $0 param plik(i)\n";
-    print "   -a podsumowanie IP\n   -b podsumowanie przeglarek\n\n";
+    print "
+ -a unikalne IP
+ -b godziny IP
+ -c najczestsze IP\n\n"
 }
 
 
-
-
-if (defined $opts{a}) { $pattern = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'; ip_num @ARGV }
-elsif (defined $opts{b}) { $pattern = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'; time_of_day }
+if (defined $opts{a}) { $pattern = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'; uniq_ip @ARGV }
+elsif (defined $opts{b}) { $pattern = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'; time_of_day @ARGV }
 elsif (defined $opts{c}) { $pattern = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'; ip_max @ARGV }
+elsif (defined $opts{d}) {link_freq @ARGV }
 else { help }
 
 
