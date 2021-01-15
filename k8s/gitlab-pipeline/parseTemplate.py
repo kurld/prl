6a59ff902e3dde3fa91de16599b7e488a358f16d
M0107872 <m0107872@dest-wrl00.itc.global.mahle>
2021-01-15 11:24:50 +0100

    parser

---
+++ k8s/gitlab-pipeline/parseTemplate.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# Usage: parseTemplate.py template.yaml values.ini
+# values in ini format separated by newline, no spaces between '='
+
+import jinja2
+import sys
+from subprocess import check_output
+
+
+sourceTemplate = sys.argv[1] 
+sourceValues = sys.argv[2]
+toRender={}
+
+with open(sourceTemplate, 'r') as f:
+  t = f.read()
+f.close()
+
+template = jinja2.Template(t)
+
+with open(sourceValues, 'r') as f:
+  val = f.read().splitlines()
+f.close()
+  
+for i in val:
+  (k, v) = i.split("=")
+  toRender[k] = v
+
+parsed = template.render(toRender)
+
+
+targetTemplate = 'parsed_' + (check_output(['basename', sourceTemplate], universal_newlines=True)).rstrip()
+
+with open(targetTemplate, 'w') as fh:
+  fh.write(parsed)
+  fh.write('\n')
+fh.close()
+
